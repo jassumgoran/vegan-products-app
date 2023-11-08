bbb16e88930ab577426aaa2a80e623acc6c0d5f3
Goran Iliev <jassumgoran@gmail.com>
2023-11-08 12:38:52 +0100

    Implemented search request debouncing

--- screens/ProductListScreen/index.tsx
+++ screens/ProductListScreen/index.tsx
@@ -3,7 +3,7 @@
  * @todo Replace hardcoded strings with localized strings for internationalization.
  */
 
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { ActivityIndicator, View, TextInput } from "react-native";
 import Screen from "../Screen";
 import { RootStackParamList } from "../../types";
@@ -16,17 +16,37 @@ import Pagination from "../../components/Pagination";
 import EmptyState from "../../components/EmptyState";
 import colors from "../../constants/colors";
 
+const DEBOUNCE_DELAY = 250;
+
 type Props = NativeStackScreenProps<RootStackParamList, "ProductList">;
 
 const ProductListScreen: React.FC<Props> = ({ navigation }) => {
   const [search, setSearch] = useState<string>("");
+  const [query, setQuery] = useState<string>("");
   const { loading, products, pagination, showEmptyState, fetchProducts } =
-    useProducts(search);
+    useProducts(query);
+  const debounceSearchTimerRef = useRef<NodeJS.Timeout | null>(null);
 
   const handleProductClick = (product: Product) => {
     navigation.navigate("ProductDetails", {
       product,
     });
+  };
+
+  /**
+   * Handles search text changes with debouncing to optimize performance by reducing redundant network requests.
+   *
+   * @param {string} searchText - The search text entered by the user.
+   * @todo Move this logic to a hook for future reusability
+   */
+  const handleSearchTextChange = (searchText: string) => {
+    setSearch(searchText);
+    if (debounceSearchTimerRef.current) {
+      clearTimeout(debounceSearchTimerRef.current);
+    }
+    debounceSearchTimerRef.current = setTimeout(() => {
+      setQuery(searchText);
+    }, DEBOUNCE_DELAY);
   };
 
   return (
@@ -38,7 +58,7 @@ const ProductListScreen: React.FC<Props> = ({ navigation }) => {
             placeholder="Search for products"
             clearButtonMode="always"
             value={search}
-            onChangeText={setSearch}
+            onChangeText={handleSearchTextChange}
           />
         </View>
         {loading ? (
