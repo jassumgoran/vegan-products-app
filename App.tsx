612c3ef60160b7c0648d229cfc263e785be59331
Goran Iliev <jassumgoran@gmail.com>
2023-11-08 09:32:39 +0100

    Changed android package name and custom topbar icon

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { FontAwesome5 } from "@expo/vector-icons";
 import ProductListScreen from "./screens/ProductListScreen";
 import ProductDetailsScreen from "./screens/ProductDetailsScreen";
 import { RootStackParamList } from "./types";
@@ -8,12 +9,37 @@ import "./api";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
+function LogoTitle() {
+  return <FontAwesome5 name="leaf" size={24} color="green" />;
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="ProductList">
-        <Stack.Screen name="ProductList" component={ProductListScreen} />
-        <Stack.Screen name="ProductDetails" component={ProductDetailsScreen} />
+      <Stack.Navigator
+        initialRouteName="ProductList"
+        screenOptions={{
+          headerTintColor: "green",
+          headerStyle: {
+            backgroundColor: "lightyellow",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="ProductList"
+          component={ProductListScreen}
+          options={{
+            title: "Products",
+            headerTitle: () => <LogoTitle />,
+          }}
+        />
+        <Stack.Screen
+          name="ProductDetails"
+          component={ProductDetailsScreen}
+          options={{
+            title: "Details",
+          }}
+        />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
